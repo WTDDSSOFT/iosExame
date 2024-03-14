74b1d270f4d1d91aaad2534fbae4dd7d0137cb41
santos <william.santos@bringglobal.com>
2024-03-14 17:16:33 -0400

    Initial Commit

---
+++ IOSExam/IOSExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSExamApp.swift
+//  IOSExam
+//
+//  Created by willaim santos on 14/03/24.
+//
+
+import SwiftUI
+
+@main
+struct IOSExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
